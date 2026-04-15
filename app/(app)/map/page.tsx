12b2fdaebf8f51ac41fa0b40f3f7445c24030d1c
ClaudeTheo <265861657+ClaudeTheo@users.noreply.github.com>
2026-04-15 18:59:03 +0200

    Add mobile bottom spacing to map page

--- app/(app)/map/page.tsx
+++ app/(app)/map/page.tsx
@@ -5,7 +5,7 @@ import { PageHeader } from "@/components/ui/page-header";
 
 export default function MapPage() {
   return (
-    <div className="space-y-4 lg:relative lg:left-1/2 lg:w-[min(calc(100vw-4rem),1100px)] lg:-translate-x-1/2 lg:space-y-6">
+    <div className="space-y-4 pb-24 lg:relative lg:left-1/2 lg:w-[min(calc(100vw-4rem),1100px)] lg:-translate-x-1/2 lg:space-y-6">
       <PageHeader
         title="Quartierskarte"
         subtitle="Ihr Quartier auf der Karte"
